d7572eff8c3eb8a64a0ae896ee46eb6c50ef4de5
SohelDewan <sohelds241@gmail.com>
2024-04-13 15:00:52 +0600

    dynamic data load and card creations

--- src/components/EstateSection.jsx
+++ src/components/EstateSection.jsx
@@ -1,10 +1,43 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+const EstateSection = ({ estates }) => {
+  const {
+    location,
+    facilities,
+    image,
+    estate_title,
+    segment_name,
+    description,
+    view_property_button,
+  } = estates;
 
-const EstateSection = () => {
-    return (
-        <div>
-            <h2>EstateSection</h2>
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="image" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          <h2>{estate_title}</h2>
+          <div className="badge badge-secondary">{segment_name}</div>
+        </h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">{location}</div>
+          <div className="badge badge-outline">{facilities}</div>
         </div>
-    );
+    <button className="px-8 py-2 bg-[#30B6EC] text-white rounded-3xl">{view_property_button}</button>
+      </div>
+    </div>
+  );
 };
 
+EstateSection.propTypes = {
+  estates: PropTypes.shape({
+    estate_title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    segment_name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default EstateSection;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,25 @@
 // import Navbar from "./Navbar";
+import { useLoaderData } from "react-router-dom";
 import Slider from "../Slider";
 import EstateSection from "./EstateSection";
 
 const Home = () => {
+    const estates = useLoaderData();
+    console.log(estates)
     return (
         <div>
             {/* <Navbar></Navbar> */}
             <Slider></Slider>
-            <EstateSection></EstateSection>
+            {/* <EstateSection></EstateSection> */}
+            <div className="grid md:grid-cols-3 container mx-auto my-8 gap-4">
+
+            {
+                estates.map(est => <EstateSection
+                            key={est.id}
+                            estates={est}
+                        ></EstateSection>)
+                    }
+            </div>
         </div>
     );
 };
